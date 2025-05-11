f76e8aa2db24f8c396f3b9ec644f979e98184e5f
Felipe Alves de Jesus <fajreyy.bh@gmail.com>
2025-05-11 17:01:47 -0300

    Update Mobile App Layout

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -4,7 +4,7 @@ import {
   ThemeProvider,
 } from "@react-navigation/native";
 import { useFonts } from "expo-font";
-import { Stack } from "expo-router";
+import { Slot, Stack, useRouter, useSegments } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
@@ -15,11 +15,33 @@ import { useColorScheme } from "@/hooks/useColorScheme";
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
+function useProtectedRoute(isAuthenticated: boolean) {
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    const inAuthGroup = segments[0] === "(auth)";
+
+    if (!isAuthenticated && !inAuthGroup) {
+      // Redirect to the sign-in page.
+      router.replace("/auth/login");
+    } else if (isAuthenticated && inAuthGroup) {
+      // Redirect away from the sign-in page.
+      router.replace("/");
+    }
+  }, [isAuthenticated, segments]);
+}
+
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme() ?? "light";
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
+
+  // TODO: Replace with actual auth state
+  const isAuthenticated = false;
+
+  useProtectedRoute(isAuthenticated);
 
   useEffect(() => {
     if (loaded) {
@@ -33,10 +55,7 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <Slot />
       <StatusBar style="auto" />
     </ThemeProvider>
   );
